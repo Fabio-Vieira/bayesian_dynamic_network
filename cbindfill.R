11c48a9ead32061383f97efcdc7b09ab8981d1ec
Fábio Vieira <f.v.generosovieira@tilburguniversity.edu>
2023-05-08 10:46:29 +0200

    Delete cbindfill.R

--- cbindfill.R
+++
@@ -1,7 +0,0 @@
-cbind.fill <- function(...){
-  nm <- list(...) 
-  nm <- lapply(nm, as.matrix)
-  n <- max(sapply(nm, nrow)) 
-  do.call(cbind, lapply(nm, function (x) 
-    rbind(x, matrix(, n-nrow(x), ncol(x))))) 
-}
